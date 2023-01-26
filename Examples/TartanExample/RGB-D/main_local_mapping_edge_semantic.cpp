3967b3a6cabe9d8a3901ab3d93a9553545a23727
zeroAska <trotwoodray@gmail.com>
2023-01-26 01:55:53 -0500

    tuning

--- Examples/TartanExample/RGB-D/main_local_mapping_edge_semantic.cpp
+++ Examples/TartanExample/RGB-D/main_local_mapping_edge_semantic.cpp
@@ -13,13 +13,35 @@
 #include "Mapping/bkioctomap.h"
 #include "utils/Calibration.hpp"
 #include "utils/ImageRGBD.hpp"
+#include "utils/ImageStereo.hpp"
 #include "dataset_handler/TartanAirHandler.hpp"
 #include "dataset_handler/PoseLoader.hpp"
 
 using namespace std;
 using namespace boost::filesystem;
 
-typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatrixXf_row;
+typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+    MatrixXf_row;
+
+
+void write_to_geotype_pcd(const cvo::CvoPointCloud & pc,
+                          const std::string & name) {
+  if (pc.num_geometric_types() < 2)
+    return;
+    
+  pcl::PointCloud<pcl::PointXYZL> pcd;
+  for (int i = 0; i < pc.size(); i++) {
+    pcl::PointXYZL p;
+    p.getVector3fMap() = pc.at(i);
+    int l;
+    pc.geometry_type_at(i).maxCoeff(&l);
+    p.label = (uint32_t) l;
+    pcd.push_back(p);
+  }
+  pcl::io::savePCDFileASCII(name ,pcd); 
+  std::cout << "Finished write to label pcd" << std::endl; 
+}
+
 
 void map_to_pc(  semantic_bki::SemanticBKIOctoMap & map,
                  cvo::CvoPointCloud & pc){
@@ -68,7 +90,7 @@ int main(int argc, char *argv[]) {
 
   string cvo_param_file(argv[2]);
   string calib_file;
-  calib_file = string(argv[1] ) +"/cvo_calib.txt"; 
+  calib_file = string(argv[1] ) +"/cvo_calib_stereo.txt"; 
   //std::string output_dir(argv[3]);
   int start_frame = stoi(argv[3]);
   int num_frames = stoi(argv[4]);
@@ -79,20 +101,20 @@ int main(int argc, char *argv[]) {
   std::cout<<" cycle through the directory\n";
   int total_num = 0;
   int last_frame = std::min(start_frame+num_frames-1, total_iters-1);
-  tum.set_start_index(start_frame);
-  cvo::Calibration  calib(calib_file, cvo::Calibration::RGBD);
+  tartan.set_start_index(start_frame);
+  cvo::Calibration  calib(calib_file, cvo::Calibration::STEREO);
 
   // Mapping
   // Set parameters
   int block_depth = 1;
   double sf2 = 1.0;
-  double ell = 0.075;
+  double ell = 0.5;
   float prior = 0.0f;
   float var_thresh = 1.0f;
-  double free_thresh = 0.85;
+  double free_thresh = 0.8;
   double occupied_thresh = 0.95;
-  double resolution = 0.02;
-  double free_resolution = 0.02;
+  double resolution = 0.1;
+  double free_resolution = 0.1;
   double ds_resolution = -1;
   double max_range = -1;
 
@@ -111,20 +133,18 @@ int main(int argc, char *argv[]) {
     
     std::cout << "Read Frame " << i+start_frame << std::endl;
     //pc_vec[i].read_cvo_pointcloud_from_file(f);
-    cv::Mat source_rgb, source_dep, source_semantics;
-    tum.read_next_rgbd(source_rgb, source_dep, num_class, source_semantics);
-    std::vector<uint16_t> source_dep_data(source_dep.begin<uint16_t>(), source_dep.end<uint16_t>());
+    cv::Mat source_left, source_right;
+    std::vector<float> semantics;
+    tartan.read_next_stereo(source_left, source_right, num_class, semantics);
 
-    //std::shared_ptr<cvo::Frame> source(new cvo::Frame(start_frame, source_rgb, source_dep,
-    //19, semantics_source, 
-    //                                                  calib, 1));
-    std::shared_ptr<cvo::ImageRGBD<uint16_t>> source_raw(new cvo::ImageRGBD(source_rgb, source_dep_data));
-    std::shared_ptr<cvo::CvoPointCloud> pc_original(new cvo::CvoPointCloud(*source_raw,
-                                                                           calib
-                                                                           //,cvo::CvoPointCloud::CANNY_EDGES
-                                                                           ));
+    std::shared_ptr<cvo::ImageStereo> source_stereo(new cvo::ImageStereo(source_left, source_right, num_class, semantics));
+    std::shared_ptr<cvo::CvoPointCloud> pc_with_semantics(new cvo::CvoPointCloud(*source_stereo,
+                                                                                 calib
+                                                                                 ,cvo::CvoPointCloud::DSO_EDGES
+                                                                                 ));
 
 
+    /*
     cvo::CvoPointCloud pc_with_semantics(pc_original->num_features(), num_class);
     pc_with_semantics.reserve(pc_original->size(), pc_original->num_features(), num_class);
     for (int k = 0; k < pc_original->size(); k++) {
@@ -136,11 +156,12 @@ int main(int argc, char *argv[]) {
       pc_with_semantics.add_point(k, pc_original->at(k), pc_original->feature_at(k),
                                   label, geometric_type);
     }
+    */
     
     // transform point cloud
     Eigen::Matrix4f transform = poses[i].cast<float>();
     cvo::CvoPointCloud transformed_pc(5,num_class);
-    cvo::CvoPointCloud::transform(transform,pc_with_semantics, transformed_pc);
+    cvo::CvoPointCloud::transform(transform,*pc_with_semantics, transformed_pc);
     pc_full += transformed_pc;
 
     semantic_bki::point3f origin;
@@ -151,7 +172,7 @@ int main(int argc, char *argv[]) {
     // insert point cloud
     map_csm.insert_pointcloud_csm(&transformed_pc, origin, ds_resolution, free_resolution, max_range);
     transformed_pc.write_to_color_pcd(std::to_string(i+start_frame)+".pcd");
-    tum.next_frame_index();
+    tartan.next_frame_index();
     
   }
   
@@ -162,6 +183,8 @@ int main(int argc, char *argv[]) {
   //pc_vec[0].write_to_color_pcd(output_dir + "/" + "input_color.pcd");
   //pc_vec[0].write_to_label_pcd(output_dir + "/" + "input_semantics.pcd");
   pc_map.write_to_color_pcd("map.pcd");
+  pc_map.write_to_label_pcd("map_label.pcd");
+  write_to_geotype_pcd(pc_map, "map_geotype.pcd");
   pc_full.write_to_color_pcd("stacked_pc.pcd");
   //cloud_out.write_to_label_pcd(output_dir + "/" + "test_semantics.pcd");
 
