e8197023c49a5472e85461a8d255cfd534018d1c
zeroAska <trotwoodray@gmail.com>
2023-01-27 02:08:15 -0500

    add config

---
+++ scripts/tartan_stereo_bki_semantic.bash
@@ -0,0 +1,5 @@
+cd build &&
+make -j &&
+cd .. &&
+#gdb -ex run --args \
+./build/bin/TartanRGBDExample_local_mapping /home/rayzhang/media/Samsung_T5/tartanair/abandonedfactory/Easy/P001/ Examples/TartanExample/cvo_outdoor_params.yaml 0 10 /home/rayzhang/media/Samsung_T5/tartanair/abandonedfactory/Easy/P001/cvo_tartan_easy_outdoor_abandonedfactory.txt 19
