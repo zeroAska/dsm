95acd74b419bf7db5f92f2896c7bb044d5e64f8b
zeroAska <trotwoodray@gmail.com>
2023-02-05 01:16:55 -0500

    debugging

--- scripts/tartan.bash
+++ scripts/tartan.bash
@@ -7,15 +7,17 @@ cd .. && \
 for difficulty in Easy
 do
 #for i in westerndesert  #hospital abandonedfactory_night #endofworld neighborhood oldtown  
-#for i in  gascola soulcity abandonedfactory  seasidetown  seasonsforest seasonsforest_winter hospital endofworld neighborhood oldtown neighborhood  
-for i in  hospital #seasidetown  soulcity
+#for i in  gascola soulcity abandonedfactory  seasidetown  seasonsforest seasonsforest_winter hospital endofworld  
+
+for i in abandonedfactory  gascola  hospital  seasidetown  seasonsforest  seasonsforest_winter  soulcity	
+#for i in  hospital #seasidetown  soulcity
 	 do
 		 folder=tartan_${difficulty}_${i}_$date
 	 rm -rf tartan_${difficulty}_${i}_$date
 	mkdir -p tartan_${difficulty}_${i}_${date}
 #gdb -ex run  --args \
 #./build/bin/TartanRGBDExample /home/rayzhang/media/Samsung_T5/tartanair/$i/${difficulty}/P001 Examples/TartanExample/cvo_outdoor_params.yaml Examples/TartanExample/outdoor_settings.txt 0 test_${difficulty}_$i.txt #> log_tartan_${difficulty}_${i}.txt
-./build/bin/TartanRGBDExample /home/rayzhang/media/Samsung_T5/tartanair/$i/${difficulty}/P001 Examples/TartanExample/cvo_outdoor_params.yaml Examples/TartanExample/no_qt_settings.txt 0 test_${difficulty}_$i.txt #> log_tartan_${difficulty}_${i}.txt
+./build/bin/TartanRGBDExample_no_qt /home/rzh/media/sdc1/rzh/tartanair/$i/${difficulty}/P001 Examples/TartanExample/cvo_outdoor_params.yaml Examples/TartanExample/no_qt_settings.txt 0 test_${difficulty}_$i.txt #> log_tartan_${difficulty}_${i}.txt
 mv *.pcd $folder
 mv *_graph.txt $folder
 mv test_${difficulty}_$i.txt $folder/
--- scripts/tartan_rgbd.bash
+++ scripts/tartan_rgbd.bash
@@ -1,7 +1,27 @@
 clear
+date=$1
 cd build && \
 make -j && \
 cd .. && \ 
-# gdb -ex r --args \
-# gdb --args \
-./build/bin/TartanRGBDExample /home/rayzhang/media/Samsung_T5/tartanair/abandonedfactory/Easy/P001 Examples/TartanExample/cvo_outdoor_params.yaml Examples/TartanExample/outdoor_settings.txt 0 test_tartan.txt
+for difficulty in Easy
+do
+for i in abandonedfactory  gascola  hospital  seasidetown  seasonsforest  seasonsforest_winter  soulcity
+do
+    rm *.pcd
+    rm *_graph.txt
+    folder=tartan_${difficulty}_${i}_$date
+    rm -rf $folder
+    mkdir -p $folder
+
+#gdb -ex run  --args \
+#./build/bin/TartanRGBDExample /home/rayzhang/media/Samsung_T5/tartanair/$i/${difficulty}/P001 Examples/TartanExample/cvo_outdoor_params.yaml Examples/TartanExample/outdoor_settings.txt 0 test_${difficulty}_$i.txt #> log_tartan_${difficulty}_${i}.txt
+#gdb -ex run  --args \
+./build/bin/TartanRGBDExample_no_qt /home/rzh/media/sdc1/rzh/tartanair/$i/${difficulty}/P001 Examples/TartanExample/cvo_outdoor_params.yaml Examples/TartanExample/no_qt_semantic_settings.txt 0 tartan_${difficulty}_$i.txt  > log_tartan_${difficulty}_${i}.txt
+mv *.pcd $folder
+mv *_graph.txt $folder
+mv tartan_${difficulty}_${i}.txt $folder/rkhs_slam.txt
+mv covisResult.txt $folder
+mv log_tartan_${difficulty}_${i}.txt $folder
+sleep 2
+done
+done
